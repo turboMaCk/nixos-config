124a95476252862a6c88b7cd0c4e69227e6f4a4a
Marek Fajkus <marek.faj@gmail.com>
2018-09-16 23:46:42 +0200

    add code

---
+++ configuration.nix
@@ -0,0 +1,211 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  inherit (pkgs) callPackage;
+  wire-desktop = callPackage ./wire-desktop.nix {};
+
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # :'(
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "nixos-mainframe"; # Define your hostname.
+    networkmanager.enable = true;
+    wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    vim_configurable
+    firefox
+    chromium
+    git
+    emacs
+    pass
+    browserpass
+    rxvt_unicode
+    xterm
+    dmenu
+    haskellPackages.xmobar
+    docker
+    dropbox
+    screenfetch
+    feh
+    htop
+    spotify
+    gnupg
+    slack
+    python
+    wire-desktop
+    transmission-gtk
+    vlc
+  ];
+
+  # Zsh
+  programs.zsh.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+
+  # fonts 
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      anonymousPro
+      corefonts
+      dejavu_fonts
+      font-droid
+      freefont_ttf
+      google-fonts
+      inconsolata
+      liberation_ttf
+      powerline-fonts
+      source-code-pro
+      terminus_font
+      ttf_bitstream_vera
+      ubuntu_font_family
+    ];
+  };
+
+
+  # Enable touchpad support.
+  # services.xserver.
+  
+
+   # X11 settings
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbOptions = "eurosign:e";
+    dpi = 130;
+    libinput.enable = false; # touchbar
+  
+    # Display Manager
+    displayManager = {
+       sddm.enable = true;
+       lightdm.enable = false;
+    };
+  
+    desktopManager = {
+      plasma5.enable = true;
+      gnome3.enable = false;
+      xfce.enable = false;
+      default = "plasma5";
+   };
+  
+    # Xmonad
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+         haskellPackages.xmonad-contrib
+         haskellPackages.xmonad-extras
+         haskellPackages.xmonad
+      ];
+    };
+ };
+
+  # compton
+  services.compton = {
+    enable          = true;
+    fade            = true;
+    inactiveOpacity = "0.9";
+    shadow          = true;
+    fadeDelta       = 4;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.marek = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" "vboxusers" "docker" ];
+    shell = pkgs.zsh;
+  };
+
+  users.users.marek.packages = [
+    pkgs.steam
+  ];
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  ## SYSTEMD
+  
+  systemd.user.services."urxvtd" = {
+    enable = true;
+    description = "rxvt unicode daemon";
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.rxvt_unicode ];
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStart = "${pkgs.rxvt_unicode}/bin/urxvtd -q -o";
+  };
+
+  # Virtualization
+
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
+
+}
